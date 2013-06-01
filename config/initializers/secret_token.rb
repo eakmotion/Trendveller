529bb8c8dbd55091d31e6876b0773866010fe12f
eakmotion <eakpun@gmail.com>
2013-06-01 16:44:16 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trendveller::Application.config.secret_token = 'ed08f4956df4be096332f7000aa6a93533f1bc3098ed5273272433b532b60c7e98c245758070459c5990b3757a536510f1b1de47084bad1b0fd452787f011d7c'
